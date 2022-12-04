8564ac7f8c1c31ebeba8748355aaa78c4a3b4deb
reddiridabl666 <reddiridabl666@gmail.com>
2022-12-04 10:35:51 +0000

    Remade tile again

--- common/logic/include/base/Tile.hpp
+++ common/logic/include/base/Tile.hpp
@@ -3,57 +3,43 @@
 #include <cstddef>
 
 namespace DnD {
-template <typename T>
-struct Vector {
-    T x = 0;
-    T y = 0;
+struct Tile {
+    size_t x = 0;
+    size_t y = 0;
 
-    bool operator==(const Vector& other) const {
+    bool operator==(const Tile& other) const {
         return x == other.x && y == other.y;
     }
 
-    size_t distance(const Vector& other) {
+    size_t distance(const Tile& other) {
         return 0;
     }
 
-    Vector operator+(const Vector& other) {
-        return Vector{x + other.x, y + other.y};
+    template <typename VectorType>
+    Tile operator+(const VectorType& other) {
+        return Tile{x + other.x, y + other.y};
     }
 
-    Vector operator-(const Vector& other) {
-        return Vector{x - other.x, y - other.y};
+    template <typename VectorType>
+    Tile operator-(const VectorType& other) {
+        return Tile{x - other.x, y - other.y};
     }
 
-    Vector& operator+=(const Vector& other) {
+    template <typename VectorType>
+    Tile& operator+=(const VectorType& other) {
         *this = *this + other;
         return *this;
     }
 
-    Vector& operator-=(const Vector& other) {
+    template <typename VectorType>
+    Tile& operator-=(const VectorType& other) {
         *this = *this - other;
         return *this;
     }
 };
 
-using Tile = Vector<size_t>;
-
-using Offset = Vector<long long>;
-
-inline Tile operator+(const Tile& tile, const Offset& offset) {
-    return Tile{tile.x + offset.x, tile.y + offset.y};
-}
-
-inline Tile operator-(const Tile& tile, const Offset& offset) {
-    return Tile{tile.x - offset.x, tile.y - offset.y};
-}
-
-inline Tile& operator+=(Tile& tile, const Offset& offset) {
-    tile = tile + offset;
-    return tile;
-}
-
-inline Tile& operator-=(Tile& tile, const Offset& offset) {
-    tile = tile - offset;
-    return tile;
-}
+struct Offset {
+    long long x;
+    long long y;
+};
 }  // namespace DnD
