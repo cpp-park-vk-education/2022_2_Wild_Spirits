f129a7319c38377a3f110e4e1de9599730870b59
Nikita <rioskill@yandex.ru>
2022-12-22 16:28:52 +0300

    Demo preparation

--- server/gateway/src/room/Room.hpp
+++ server/gateway/src/room/Room.hpp
@@ -8,17 +8,55 @@ class Gateway;
 
 class User;
 
-class RequestProcessor {};
+#ifdef BUILD_LOGIC
+    #include <GameState.hpp>
+    #include <RoomRequestProcessor.hpp>
+#else
+namespace DnD {
+    class LogicProcessor {
+
+    };
+
+    class DemoLogicProcessor: public LogicProcessor {
+
+    };
+
+    class GameMap {
+
+    };
+
+    class DemoGameMap: public GameMap {
+    public:
+        DemoGameMap(LogicProcessor &p) {}
+    };
+};
+
+class Room;
+
+class RoomSideProcessor {
+public:
+    RoomSideProcessor(Room &room, DnD::LogicProcessor &p) {}
+    bool acceptRequest(std::string request_string) {
+        
+    }
+};
+#endif
 
 class Room {
 protected:
     std::size_t _id;
-    RequestProcessor processor;
+
+    std::unique_ptr<DnD::LogicProcessor> logic_processor;
+    std::unique_ptr<DnD::GameMap> game_map;
+
+    RoomSideProcessor processor;
 
 public:
     Gateway &gateway;
 
-    Room(std::size_t id, Gateway &gateway): _id(id), gateway(gateway) {}
+    Room(std::size_t id, Gateway &gateway):
+        _id(id), gateway(gateway), logic_processor(std::make_unique<DnD::DemoLogicProcessor>()),
+        game_map(std::make_unique<DnD::DemoGameMap>(*logic_processor)), processor(*this, *logic_processor) {}
 
     virtual void addUser(User*) = 0;
 
--- server/gateway/src/room/RoomImpl.cpp
+++ server/gateway/src/room/RoomImpl.cpp
@@ -4,6 +4,8 @@
 
 RoomImpl::RoomImpl(User *user, std::size_t id, Gateway &gateway): Room(id, gateway), DM(user) {
     players.push_back(user);
+
+
 }
 
 void RoomImpl::addUser(User *player) {
@@ -32,19 +34,5 @@ void RoomImpl::broadcast(const std::string &message) {
 void RoomImpl::processRequest(std::size_t user_id, const std::string &request) {
     // broadcast(request);
 
-    std::size_t delimiter_pos = request.find(':');
-
-    std::string action = request.substr(0, delimiter_pos);
-    std::string content = request.substr(delimiter_pos + 1);
-
-    if (action == "get") {
-        auto image = gateway.image_storage.getImage(content);
-        if (image)
-            sendDM(image->getBase64());
-        else
-            sendDM("image does not exist");
-    } else {
-        auto hash = gateway.image_storage.saveImage(content, "png");
-        broadcast(hash);
-    }
+    processor.acceptRequest(request);
 }
