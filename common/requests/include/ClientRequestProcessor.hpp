f62feb6d77a475d8cd6549a185c95721c2361ce6
Mihail <mihail.latyshov2@mail.ru>
2022-12-24 22:44:46 +0300

    sync register

--- common/requests/include/ClientRequestProcessor.hpp
+++ common/requests/include/ClientRequestProcessor.hpp
@@ -1,41 +1,42 @@
 #pragma once
 
-
-#include "sendaccept.hpp"
+#include "ClientInterLayer.hpp"
 #include "ClientProcessorEngine.hpp"
 #include "GameStateChanger.hpp"
-#include "ClientInterLayer.hpp"
 #include "TurnOrder.hpp"
+#include "sendaccept.hpp"
 
-#include <EventLoop.hpp>
-#include <ClientConnection.hpp>
 #include <Classes/Room.h>
-//TODO: При создании комнаты установить на сервере id клиента как DM
-//TODO: Connection должен возвращать id комнаты, как и getROoms
+#include <ClientConnection.hpp>
+#include <EventLoop.hpp>
+// TODO: При создании комнаты установить на сервере id клиента как DM
+// TODO: Connection должен возвращать id комнаты, как и getROoms
 
+#include <barrier>
 #include <thread>
 
-class PlayersID{
+class PlayersID {
 private:
     std::unordered_map<std::size_t, std::size_t> client_table;
     std::unordered_map<std::size_t, std::size_t> id_table;
-public:
-    PlayersID(): client_table(1), id_table(1){client_table.insert(std::make_pair(1, 1)), client_table.insert(std::make_pair(1, 1));}
-    std::size_t getPlayerId(std::size_t id){return id_table[id];}
-    std::size_t getClientId(std::size_t player_name){return client_table[player_name];}
 
+public:
+    PlayersID() : client_table(1), id_table(1) {
+        client_table.insert(std::make_pair(1, 1)), client_table.insert(std::make_pair(1, 1));
+    }
+    std::size_t getPlayerId(std::size_t id) { return id_table[id]; }
+    std::size_t getClientId(std::size_t player_name) { return client_table[player_name]; }
 };
 
-
-class ClientSideProcessor: public RequestAcceptor, public RequestSender{
+class ClientSideProcessor : public RequestAcceptor, public RequestSender {
 public:
     PlayersID playersId;
-    std::size_t getPlayerId(){return playersId.getPlayerId(_client_id);}
-    std::size_t getClientId(){return _client_id;}
-    ClientSideProcessor(DnD::GameState &gamestate, DnD::GameMap& map, DnD::TurnOrder& order);
-    ClientSideProcessor(DnD::GameState &gamestate, unsigned int client_id);
+    std::size_t getPlayerId() { return playersId.getPlayerId(_client_id); }
+    std::size_t getClientId() { return _client_id; }
+    ClientSideProcessor(DnD::GameState& gamestate, DnD::GameMap& map, DnD::TurnOrder& order);
+    // ClientSideProcessor(DnD::GameState& gamestate, unsigned int client_id);
 
-    bool sendRequest(LM::Action &action) override;
+    bool sendRequest(LM::Action& action) override;
     std::string sendRequest(std::string request);
     bool getImage(std::string_view hash, std::shared_ptr<std::string>);
     bool acceptRequest(string request_string) override;
@@ -47,11 +48,12 @@ public:
     bool Register(std::string login, std::string password);
     bool Login(std::string, std::string password);
     bool isAuthorized() const;
-    //DM Methods
+    // DM Methods
     bool checkUnappliedChanges() const;
 
     void start();
     void stop();
+
 private:
     BoostEventLoop loop;
     bool is_connected;
@@ -69,11 +71,13 @@ private:
     InterlayerBuffer buffer;
     DnD::GameMap& _map;
     DnD::TurnOrder& _order;
-    bool SendChangesRequest(LM::Action &action);
+    bool SendChangesRequest(LM::Action& action);
     bool ApplyChanges(string changes);
     std::string_view ImageRequest(std::string_view image_hash);
     Header getHeader(std::string request);
     bool ApplicationRequest(queue changes);
 
     std::jthread loop_thread;
+    static inline std::function<void()> on_completion = []() { std::cout << "BC!\n"; };
+    std::barrier<decltype(on_completion)> barrier = std::barrier(2, on_completion);
 };
--- common/requests/src/ClientRequestProcessor.cpp
+++ common/requests/src/ClientRequestProcessor.cpp
@@ -6,81 +6,79 @@
 
 #include <iostream>
 
-//Interface methods
-bool ClientSideProcessor::acceptRequest(string request_string){
+// Interface methods
+bool ClientSideProcessor::acceptRequest(string request_string) {
     bool state = true;
-    
+
     std::cout << "request_string: " << request_string << std::endl;
-    if(request_string.starts_with("room_id")){
+    if (request_string.starts_with("room_id")) {
         _room_id = stoi(request_string.substr(request_string.find(":") + 1));
         return true;
     }
-    if(request_string == "Registered successfully"){
+    if (request_string == "Registered successfully") {
         setAuthorized();
+        std::cout << "AR\n";
+        barrier.arrive();
         return true;
     }
-    if(request_string == "Logined successfully"){
+    if (request_string == "Authorized successfully") {
         setAuthorized();
+        std::cout << "AR\n";
+        barrier.arrive();
         return true;
     }
-    if(request_string.starts_with("Authorization error")){
+    if (request_string.starts_with("Authorization error")) {
         setUnAuthorized();
+        std::cout << "AR\n";
+        barrier.arrive();
         return false;
     }
-    switch(getHeader(request_string)){
+    switch (getHeader(request_string)) {
 
-        case action:
-            throw HeaderException("Invalid header for accept");
-        case img_request:
-            throw HeaderException("Invalid header for accept");
-        case room_changes:
-            state = ApplicationRequest(engine.getSetterQueue(request_string));
-            break;
-        case apply_request:
-            state = ApplyChanges(request_string);
-            break;
+        case action: throw HeaderException("Invalid header for accept");
+        case img_request: throw HeaderException("Invalid header for accept");
+        case room_changes: state = ApplicationRequest(engine.getSetterQueue(request_string)); break;
+        case apply_request: state = ApplyChanges(request_string); break;
     }
     return state;
 }
 
-bool ClientSideProcessor::sendRequest(LM::Action &action){
-    return SendChangesRequest(action);
-}
+bool ClientSideProcessor::sendRequest(LM::Action& action) { return SendChangesRequest(action); }
 
 #include <iostream>
 
-std::string ClientSideProcessor::sendRequest(std::string request){
+std::string ClientSideProcessor::sendRequest(std::string request) {
     std::cout << "sending: " << request << std::endl;
     bool state = true;
     std::string response_buffer;
-    connection ->sendMessage(request, [](bool answer){
-        std::cout << "answer: " << answer << std::endl;
-    });
+    connection->sendMessage(request, [](bool answer) { std::cout << "answer: " << answer << std::endl; });
     return response_buffer;
 }
 
 bool ClientSideProcessor::getImage(std::string_view hash, std::shared_ptr<std::string> image_string_buffer) {
     bool state = true;
     std::string_view image_view = ImageRequest(hash);
-    image_string_buffer -> assign(image_view);
+    image_string_buffer->assign(image_view);
     return state;
 }
 
-//Internal methods
+// Internal methods
 
-bool ClientSideProcessor::SendChangesRequest(LM::Action &action) {
+bool ClientSideProcessor::SendChangesRequest(LM::Action& action) {
 
     bool state = true;
     std::string action_string = engine.handler->actionString(action);
-    std::function<void(bool)> handler = [&state](bool status){state = status;}; //TODO: Сделать коды ответа в handler
-    connection -> sendMessage(engine.getRequestString(action_string, Header::action), handler);
+    std::function<void(bool)> handler = [&state](bool status) {
+        state = status;
+    };    // TODO: Сделать коды ответа в handler
+    connection->sendMessage(engine.getRequestString(action_string, Header::action), handler);
     return state;
 }
 
 bool ClientSideProcessor::ApplyChanges(string changes) {
     bool state = true;
     queue setters = engine.getSetterQueue(changes);
-    while(!setters.empty()){
+    while (!setters.empty()) {
         json temp;
         state &= changer.makechange(temp[std::get<0>(setters.front())] = std::get<1>(setters.front()));
         setters.pop_front();
@@ -90,29 +88,31 @@ bool ClientSideProcessor::ApplyChanges(string changes) {
 
 std::string_view ClientSideProcessor::ImageRequest(std::string_view image_hash) {
     std::string_view img_view;
-    std::function<void(std::string)> handler = [&img_view](std::string img_string){img_view = img_string;};
+    std::function<void(std::string)> handler = [&img_view](std::string img_string) { img_view = img_string; };
     HeaderSerial s;
-    connection -> sendMessage(engine.generator->makeRequestString(std::string(image_hash), s(Header::img_request)), [](bool status){
+    connection->sendMessage(
+        engine.generator->makeRequestString(std::string(image_hash), s(Header::img_request)),
+        [](bool status) {
 
-    });
+        });
     return img_view;
 }
-//Interface methods
+// Interface methods
 bool ClientSideProcessor::Connection(std::string ip, std::string port) {
     bool state = true;
-    // connection = std::make_shared<ClientConnectionImpl>(ip, port, loop, std::function<void(std::string)>([this, &state](std::string message){
+    // connection = std::make_shared<ClientConnectionImpl>(ip, port, loop,
+    // std::function<void(std::string)>([this, &state](std::string message){
     //     state = acceptRequest(message);
     // }));
-    connection = ClientConnectionImpl::createConnection(ip, std::stoi(port), loop, [this](std::string message){
-        acceptRequest(message);
-    });
+    connection = ClientConnectionImpl::createConnection(
+        ip, std::stoi(port), loop, [this](std::string message) { acceptRequest(message); });
     return state;
 }
 
 LM::Room ClientSideProcessor::CreateRoom() {
     std::string creating_request = "create";
     sendRequest(creating_request);
-    return LM::Room({_room_id});
+    return LM::Room({ _room_id });
 }
 
 bool ClientSideProcessor::StartGame() {
@@ -132,71 +132,61 @@ Header ClientSideProcessor::getHeader(std::string request) {
     return deserializer(request_obj.begin().key());
 }
 
-bool ClientSideProcessor::checkUnappliedChanges() const {
-    return buffer.hasUnappliedChanges();
-}
+bool ClientSideProcessor::checkUnappliedChanges() const { return buffer.hasUnappliedChanges(); }
 
-bool ClientSideProcessor::ConnectToRoom(LM::Room room) {
-    return false;
-}
+bool ClientSideProcessor::ConnectToRoom(LM::Room room) { return false; }
 
-bool ClientSideProcessor::isAuthorized() const {
-    return is_authorized;
-}
+bool ClientSideProcessor::isAuthorized() const { return is_authorized; }
 
 bool ClientSideProcessor::Login(std::string login, std::string password) {
     std::stringstream ss;
     ss << "login:" << login << ":" << password;
     sendRequest(ss.str());
-    return true;
+    std::cout << "AR WT\n";
+    barrier.arrive_and_wait();
+    return isAuthorized();
 }
 
 bool ClientSideProcessor::Register(std::string login, std::string password) {
     std::stringstream ss;
     ss << "register:" << login << ":" << password;
     sendRequest(ss.str());
-    return true;
+    std::cout << "AR WT\n";
+    barrier.arrive_and_wait();
+    return isAuthorized();
 }
 
-ClientSideProcessor::ClientSideProcessor(DnD::GameState &gamestate, DnD::GameMap &map, DnD::TurnOrder &order): gamestate(gamestate),
-                                                                                                               changer(gamestate, map), _map(map), _order(order)\
-                                                                                                               , _room_id(0) {
+ClientSideProcessor::ClientSideProcessor(DnD::GameState& gamestate, DnD::GameMap& map, DnD::TurnOrder& order)
+    : gamestate(gamestate),
+      changer(gamestate, map),
+      _map(map),
+      _order(order),
+      _room_id(0) { }
 
-}
+std::vector<LM::Room> ClientSideProcessor::GetRooms() { return std::vector<LM::Room>(); }
 
-std::vector<LM::Room> ClientSideProcessor::GetRooms() {
-    return std::vector<LM::Room>();
-}
+void ClientSideProcessor::setAuthorized() { is_authorized = true; }
 
-void ClientSideProcessor::setAuthorized(){
-    is_authorized = true;
-}
-
-void ClientSideProcessor::setUnAuthorized(){
-    is_authorized = false;
-}
+void ClientSideProcessor::setUnAuthorized() { is_authorized = false; }
 
 // bool ClientSideProcessor::sendRequest(LM::Action &action) {
 //     return SendChangesRequest(action);
 // }
 
-//ClientSideProcessor::ClientSideProcessor(Client::GameState &gamestate): is_connected(false), _client_id(), engine(), connection(), gamestate(gamestate),
-//                                                                        changer(gamestate), buffer(){
+// ClientSideProcessor::ClientSideProcessor(Client::GameState &gamestate): is_connected(false), _client_id(),
+// engine(), connection(), gamestate(gamestate),
+//                                                                         changer(gamestate), buffer(){
 //
-//}
+// }
 
 void ClientSideProcessor::start() {
     connection->recieve();
 
-    loop_thread = std::jthread([this](){
-        loop.start();
-    });
+    loop_thread = std::jthread([this]() { loop.start(); });
 }
 
 void ClientSideProcessor::stop() {
-    connection->close([this]{
-        loop.stop();
-    });
+    connection->close([this] { loop.stop(); });
 
     loop_thread.join();
 }
